e8f2307ca0f4c11638a70fae353d77ec1e156520
Larry Jones <larry.jones@continuous.com>
2026-07-03 11:35:58 -0500

    Revised to use idiomatic code
    Additionally included a comment summarizing the implementation choice
    and "previewing" future changes.

--- tests/Expenses.Tests/KataLibraryTests.cs
+++ tests/Expenses.Tests/KataLibraryTests.cs
@@ -42,18 +42,23 @@ public class Library
         _books.Add(book.Id, book);
     }
 
-    public Option<Book> FindByIsbn(Isbn sought)
-    {
-        
-        if (_books.TryGetValue(sought, out var book))
-        {
-            return Some<Book>(book);
-        }
-        else
-        {
-            return Option<Book>.None;
-        }
-    }
+    // Idiomatic code for translating a C# value that could return `null` into an `Option` type.
+    //
+    // The `GetValueOrDefault()` call returns the sought book if it is present. If `null` is returned, the compiler
+    // will return the "default" for the type, `Book`. Since `Book` is a record, the default value will again be
+    // `null`. The call to `Optional` will translate these two values into `Option.Some<Book>` if the value is
+    // **not** `null` and into `Option<Book>.None` if no such book exists.
+    //
+    // We could have defined a `Book` to be a `record struct` instead of a `record`. The `record` type, under the 
+    // hood is actually a .NET class which has a default value of `null`. If we had used a `record struct`. we 
+    // would actually have a `struct` "under the hood" and the default value would not be `null` but a record with
+    // all "bits" initialized to zero.
+    //
+    // Sneak peak. we'll review this choice in Phase 4 when we translate the `Dictionary` to using `Map<K, V>`
+    // from LanguageExt.
+    //
+    public Option<Book> FindByIsbn(Isbn sought)  => 
+        Optional(_books.GetValueOrDefault(sought));
 }
 
 public record Book(Isbn Id, string Title);
