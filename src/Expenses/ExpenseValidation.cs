c86e0cc002c25a182fbf94e12a2948591de6014b
Larry Jones <larry.jones@continuous.com>
2026-08-08 13:53:37 -0500

    Include LangExt.Prelude to simplify access

--- src/Expenses/ExpenseValidation.cs
+++ src/Expenses/ExpenseValidation.cs
@@ -1,18 +1,20 @@
 using LanguageExt;
 
+using static LanguageExt.Prelude;
+
 namespace Expenses;
 
 public static class ExpenseValidation
 {
     public static Validation<string, decimal> ValidateAmount(decimal amount) =>
         amount > 0
-            ? Prelude.Success<string, decimal>(amount)
-            : Prelude.Fail<string, decimal>("Amount must be positive");
+            ? Success<string, decimal>(amount)
+            : Fail<string, decimal>("Amount must be positive");
 
     public static Validation<string, string> ValidateCategory(string category) =>
         !string.IsNullOrWhiteSpace(category)
-            ? Prelude.Success<string, string>(category)
-            : Prelude.Fail<string, string>("Category is required");
+            ? Success<string, string>(category)
+            : Fail<string, string>("Category is required");
 
     public static Validation<string, ValidatedEntry> Validate(decimal amount, string category) =>
         (ValidateAmount(amount), ValidateCategory(category))
