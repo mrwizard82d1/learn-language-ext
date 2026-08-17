e083555e153f3f456d040f7ec773ca7ec1096ab0
Larry Jones <larry.jones@continuous.com>
2026-08-17 18:11:48 -0500

    Better reflect test intent; but more tomorrow

--- tests/Expenses.Tests/WordFrequencyTests.cs
+++ tests/Expenses.Tests/WordFrequencyTests.cs
@@ -1,7 +1,25 @@
 namespace Expenses.Tests;
 
+using LanguageExt;
+
+public static class WordFrequency
+{
+    public static Map<string, int> Create() => new();
+    public static Option<int> Count(Map<string, int> dictionary, string toFind) => dictionary.Find(toFind);
+}
+
 public class WordFrequencyTests
 {
     [Fact]
     public void SmokeTest() => Assert.Equal(4, 2 + 2);
+
+    [Fact]
+    public void InitializedCounter_Count_ReturnsNone()
+    {
+        var dictionary = WordFrequency.Create();
+        
+        var counter = WordFrequency.Count(dictionary,"voluptatum");
+        
+        Assert.True(counter.IsNone);
+    }
 }
