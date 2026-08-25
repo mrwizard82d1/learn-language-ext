fdd1f397b06dfe37637f591b4e290487ff62aff6
Larry Jones <larry.jones@continuous.com>
2026-08-24 20:24:09 -0500

    Quick progress on phase 04 test example

--- tests/Expenses.Tests/WordFrequencyTests.cs
+++ tests/Expenses.Tests/WordFrequencyTests.cs
@@ -1,11 +1,16 @@
 namespace Expenses.Tests;
 
 using LanguageExt;
+using static LanguageExt.Prelude;
 
 public static class WordFrequency
 {
     public static Map<string, int> Create() => new();
-    public static Option<int> Count(Map<string, int> dictionary, string toFind) => dictionary.Find(toFind);
+    public static Option<int> CountOf(Map<string, int> dictionary, string toFind) => dictionary.Find(toFind);
+
+    public static Map<string, int> Count(string text) =>
+        toSeq(text.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            .Fold(Map<string, int>(), (accumulator, word) => accumulator.AddOrUpdate(word, cur => cur + 1, () => 1));
 }
 
 public class WordFrequencyTests
@@ -18,8 +23,16 @@ public class WordFrequencyTests
     {
         var dictionary = WordFrequency.Create();
         
-        var counter = WordFrequency.Count(dictionary,"voluptatum");
+        var counter = WordFrequency.CountOf(dictionary,"voluptatum");
         
         Assert.True(counter.IsNone);
+    }
+
+    [Fact]
+    public void Count_TalliesRepeatedWord()
+    {
+        var frequencies = WordFrequency.Count("the cat sat the mat the");
+        LangExtAssert.Equal(Some(3), WordFrequency.CountOf(frequencies, "the"));
+        
     }
 }
